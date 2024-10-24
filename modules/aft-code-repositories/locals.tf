9c60e7ad1bc8507ee20396b914a76ef11f81eb17
Igor D'Astolfo <igordust@amazon.it>
2024-10-24 12:26:04 +0200

    Added S3 vcs backend for account-request

--- modules/aft-code-repositories/locals.tf
+++ modules/aft-code-repositories/locals.tf
@@ -9,6 +9,7 @@ locals {
     is_github_enterprise  = lower(var.vcs_provider) == "githubenterprise" ? true : false
     is_gitlab             = lower(var.vcs_provider) == "gitlab" ? true : false
     is_gitlab_selfmanaged = lower(var.vcs_provider) == "gitlabselfmanaged" ? true : false
+    is_s3                 = lower(var.vcs_provider) == "s3" ? true : false
   }
   connection_arn = {
     bitbucket         = lower(var.vcs_provider) == "bitbucket" ? aws_codestarconnections_connection.bitbucket[0].arn : ""
@@ -17,5 +18,10 @@ locals {
     gitlab            = lower(var.vcs_provider) == "gitlab" ? aws_codestarconnections_connection.gitlab[0].arn : ""
     gitlabselfmanaged = lower(var.vcs_provider) == "gitlabselfmanaged" ? aws_codestarconnections_connection.gitlabselfmanaged[0].arn : ""
     codecommit        = "null"
+    s3                = "null"
   }
+
+  is_codestar = (local.vcs.is_bitbucket || local.vcs.is_github || local.vcs.is_github_enterprise)
+
+  s3_archive_filename = "repository.zip"
 }
